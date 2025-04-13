6798dfea784c118a71e4f8e9c2a525067453b9a4
NITHIN KUMAR <131851611+nithinkumar2004@users.noreply.github.com>
2025-04-13 23:04:58 +0530

    cdmadsss-ex1.m

--- Experiment1/cdmadsss-ex1.m
+++ Experiment1/cdmadsss-ex1.m
@@ -1,63 +1,27 @@
-% DSSS Modulation and Demodulation Example 
-% Parameters 
-data = [1 0 1 0 1 1 0 0]; % Original data signal
-spreadingCode = [1 1 0 1]; % Spreading code
-spreadingFactor = length(spreadingCode);
+% DSSS Modulation & Demodulation
+data = [1 0 1 0 1 1 0 0]; 
+code = [1 1 0 1]; 
+sf = length(code);
 
-% DSSS Modulation 
-modulatedSignal = [];
-for i = 1:length(data) 
-    chips = repmat(data(i), 1, spreadingFactor) .* spreadingCode;
-    modulatedSignal = [modulatedSignal chips];
-end 
+modSig = reshape(repmat(data, sf, 1) .* repmat(code', 1, length(data)), 1, []);
+demodSig = arrayfun(@(i) sum(modSig((i-1)*sf+1:i*sf)) >= sf/2, 1:length(data));
 
-% DSSS Demodulation 
-demodulatedSignal = [];
-for i = 1:length(modulatedSignal)/spreadingFactor 
-    chips = modulatedSignal((i-1)*spreadingFactor+1:i*spreadingFactor);
-    chipSum = sum(chips);
-    if chipSum >= spreadingFactor/2 
-        demodulatedSignal = [demodulatedSignal 1];
-    else 
-        demodulatedSignal = [demodulatedSignal 0];
-    end 
-end 
+disp('Original Data:'), disp(data)
+disp('Demodulated Data:'), disp(demodSig)
 
-% Display Results 
-disp('Original Data:');
-disp(data);
-disp('Demodulated Data:');
-disp(demodulatedSignal);
+% QPSK over Rayleigh Fading Channel
+numBits = 1e6; EbNo_dB = 10;
+tx = randi([0 3], 1, numBits);
+modSym = pskmod(tx, 4, pi/4);
 
-% Define simulation parameters 
-numBits = 1e6; % Number of bits to transmit
-EbNo_dB = 10; % Eb/No in dB
+EbNo = 10^(EbNo_dB/10); 
+noise = sqrt(1/(2*EbNo)) * (randn(size(modSym)) + 1i*randn(size(modSym)));
+rx = modSym + noise;
 
-% Generate QPSK symbols 
-txSymbols = randi([0 3], 1, numBits);
-modulatedSymbols = pskmod(txSymbols, 4, pi/4);
+chan = comm.RayleighChannel('SampleRate',1e6,'PathDelays',0,'AveragePathGains',0,'MaximumDopplerShift',30);
+faded = chan(rx.');
 
-% Add noise to the symbols 
-EbNo = 10^(EbNo_dB/10);
-noiseVar = 1 / (2 * EbNo);
-noise = sqrt(noiseVar) * (randn(size(modulatedSymbols)) + 1i * randn(size(modulatedSymbols)));
-rxSymbols = modulatedSymbols + noise;
+demod = pskdemod(faded.', 4, pi/4);
+ber = mean(tx ~= demod);
 
-% Apply Rayleigh fading channel using modern System object
-rayleighChan = comm.RayleighChannel( ...
-    'SampleRate', 1e6, ...
-    'PathDelays', 0, ...
-    'AveragePathGains', 0, ...
-    'MaximumDopplerShift', 30);
-
-fadedSymbols = rayleighChan(rxSymbols.');
-
-% Demodulate received symbols 
-demodulatedSymbols = pskdemod(fadedSymbols.', 4, pi/4);
-
-% Calculate Bit Error Rate (BER) 
-numErrors = sum(txSymbols ~= demodulatedSymbols);
-ber = numErrors / numBits;
-
-% Display results 
 fprintf('Bit Error Rate (BER): %.4f\n', ber);
