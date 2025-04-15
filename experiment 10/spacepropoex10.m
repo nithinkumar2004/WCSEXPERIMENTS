11a1f637426f4a74385ba7d6fc3714face00105d
NITHIN KUMAR <131851611+nithinkumar2004@users.noreply.github.com>
2025-04-15 16:32:48 +0530

    spacepropoex10.m

--- experiment 10/spacepropoex10.m
+++ experiment 10/spacepropoex10.m
@@ -1,24 +1,23 @@
-clc; 
-clear all; 
-close all;
-f=input('Enter the frequency in MHz: '); 
-Lamda=300/f; %calculating wavelength 
-Gt=input('enter the transmitting antenna gain in db: '); 
-Gr=input('enter the receiving antenna gain in db: '); 
-PTx=1e-3; %i.e 0.001 watt
-PTxdBm=10*log10(PTx); 
-d=1:1:1000;
-FSPL = 32.44+20*log10(d)+20*log10(f);
-disp('The Path Loss is:'); 
-disp(FSPL); 
+clc; clear; close all;
+
+f = input('Enter frequency (MHz): ');
+lambda = 300 / f;  % Wavelength in meters
+Gt = input('Enter Tx antenna gain (dB): ');
+Gr = input('Enter Rx antenna gain (dB): ');
+PTx = 1e-3;                     % Transmit power in watts
+PTx_dBm = 10 * log10(PTx * 1000);  % Convert to dBm
+d = 1:1000;                     % Distance in meters
+
+FSPL = 32.44 + 20*log10(d) + 20*log10(f);  % Free Space Path Loss (dB)
+Pr = PTx_dBm + Gt + Gr - FSPL;            % Received Power (dBm)
+
+% Plotting
 subplot(2,1,1);
-plot(d, FSPL,'r');
-xlabel('distance in meters >');
-ylabel(' Path Loss in dB')
-title('Free Space Propagation Loss'); 
-Pr =PTx+Gt+Gr- FSPL;
+plot(d, FSPL, 'r');
+xlabel('Distance (m)'); ylabel('Path Loss (dB)');
+title('Free Space Path Loss');
+
 subplot(2,1,2);
 plot(d, Pr);
-xlabel('distance in meters >');
-ylabel('Power in dB') 
-title('Received Power');
+xlabel('Distance (m)'); ylabel('Received Power (dBm)');
+title('Received Power over Distance');
